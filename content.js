2d20cd5f9fffe60a85f8e1a598364fe16ab39488
Shreyas Kapur <shreyask@mit.edu>
2017-05-03 16:58:25 -0400

    Shifts to Dweetio

--- content.js
+++
@@ -1,1 +0,0 @@
-var status = "Studying";
--- status.js
+++ status.js
@@ -1,6 +1,18 @@
-var exception = "Emergency or you're my mom.";
+var dweetioThing = 'shreyas-status';
 
 $(document).ready(function() {
-	$('.status').text(status);
-	$('.exception').text(exception);
+	// Initialize status
+	dweetio.get_latest_dweet_for(dweetioThing, function(err, dweet){
+    	var dweet = dweet[0]; // Dweet is always an array of 1
+    	setStatusText(dweet.content.status);
+	});
+
+	// Set a socket poller
+	dweetio.listen_for(dweetioThing, function(dweet) {
+		setStatusText(dweet.content.status);
+	});
 });
+
+function setStatusText(text) {
+	$('.status').text(text);
+}
