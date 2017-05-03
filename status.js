8334496ce54988930288163f29aa3495692b7aee
Shreyas Kapur <shreyask@mit.edu>
2017-05-03 17:00:54 -0400

    Bring back the disturb.

--- status.js
+++ status.js
@@ -5,14 +5,20 @@ $(document).ready(function() {
 	dweetio.get_latest_dweet_for(dweetioThing, function(err, dweet){
     	var dweet = dweet[0]; // Dweet is always an array of 1
     	setStatusText(dweet.content.status);
+    	setDisturbText(dweet.content.disturb);
 	});
 
 	// Set a socket poller
 	dweetio.listen_for(dweetioThing, function(dweet) {
 		setStatusText(dweet.content.status);
+		setDisturbText(dweet.content.disturb);
 	});
 });
 
 function setStatusText(text) {
 	$('.status').text(text);
+}
+
+function setDisturbText(text) {
+	$('.exception').text(text);
 }
